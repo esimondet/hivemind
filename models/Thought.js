2125b52914265f37041b7553a571dab3b24787ae
Edison Simondet <edison.simondet@gmail.com>
2022-02-10 16:41:19 -0600

    added Thought model and reactionCount virtual

---
+++ models/Thought.js
@@ -0,0 +1,67 @@
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const ReactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: 'We must know your reaction!',
+      maxlength: 128,
+    },
+    username: {
+      type: String,
+      required: 'Username is required!',
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+const ThoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: 'Give us your thoughts!',
+      minlength: 1,
+      maxlength: 128,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+    username: {
+      type: String,
+      required: 'Username is required!',
+    },
+
+    reactions: [],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+ThoughtSchema.virtual('reactionCount').get(function () {
+  return this.reactions.length;
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
--- models/User.js
+++ models/User.js
@@ -37,10 +37,7 @@ const UserSchema = new Schema(
 );
 
 UserSchema.virtual('friendCount').get(function () {
-  return this.friends.reduce(
-    (total, friend) => total + friend.count.length + 1,
-    0
-  );
+  return this.friends.length;
 });
 
 const User = model('User', UserSchema);
